5973d893f2b28709e52caa4f54145b7d124df741
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2021-10-11 17:02:34 +0900

    Update kde_plot.py

--- _unfinished_analysis_tools/kde_plot.py
+++ _unfinished_analysis_tools/kde_plot.py
@@ -12,8 +12,8 @@ from matplotlib import cm
 
 # Read PCA etc. complicated 2D data:
 x1, y1 = np.loadtxt("2dproj_ev_1_2.xvg",
-                    comments=("@", "#"),
-                    unpack=True)
+                    comments = ("@", "#"),
+                    unpack = True)
 
 # Sanity check:
 print ("x: ", x1)
@@ -24,25 +24,25 @@ data = np.column_stack((np.array(x1),
                         np.array(y1)))
 
 df1 = pd.DataFrame(data, 
-                   columns=['x1', 'y1'])
+                   columns = ['x1', 'y1'])
 
 # Use seaborn to calculate KDE and plot it
-xlabel="Projection on first eigenvector (nm)"
-ylabel="Projection on second eigenvector (nm)"
+xlabel = "Projection on first eigenvector (nm)"
+ylabel = "Projection on second eigenvector (nm)"
 
-ax = sns.jointplot(x=df1.x1,
-                   y=df1.y1,
-                   color='b',
-                   n_levels=12,
-		   #thresh=5,
-                   kind="kde",
-                   cmap="Reds",
-                   palette="Reds",
-                   #cmap="mako",
-	           xlim=[-7, 4],
-        	   ylim=[-4, 3],
-           	   linewidth=1.5,
-	           marginal_ticks=False,
-                   shade=True).set_axis_labels(xlabel, ylabel)
+ax = sns.jointplot(x = df1.x1,
+                   y = df1.y1,
+                   color = 'b',
+                   n_levels = 12,
+		   #thresh = 5,
+                   kind = "kde",
+                   cmap = "Reds",
+                   palette = "Reds",
+                   #cmap = "mako",
+	           xlim = [-7, 4],
+        	   ylim = [-4, 3],
+           	   linewidth = 1.5,
+	           marginal_ticks = False,
+                   shade = True).set_axis_labels(xlabel, ylabel)
 
 plt.savefig('kdeplot.pdf')
