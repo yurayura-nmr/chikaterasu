db6d96feefaffe01e2099397abf3d16a4fa17f64
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2021-10-11 16:57:37 +0900

    Update aru_kde.py

--- _unfinished_analysis_tools/aru_kde.py
+++ _unfinished_analysis_tools/aru_kde.py
@@ -1,7 +1,6 @@
 import matplotlib
 matplotlib.use('Agg')
 
-#import MDAnalysis as mda
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,8 +10,7 @@ import pandas as pd
 from numpy import histogram2d
 from matplotlib import cm
 
-# Read Mr. Shuto's data:
-
+# Read PCA etc. complicated 2D data:
 x1, y1 = np.loadtxt("2dproj_ev_1_2.xvg",
                     comments=("@", "#"),
                     unpack=True)
@@ -21,15 +19,14 @@ x1, y1 = np.loadtxt("2dproj_ev_1_2.xvg",
 print ("x: ", x1)
 print ("y: ", y1)
 
-# Using pandas
+# Pandas dataframe
 data = np.column_stack((np.array(x1), 
                         np.array(y1)))
 
 df1 = pd.DataFrame(data, 
                    columns=['x1', 'y1'])
 
-# Seaborn
-
+# Use seaborn to calculate KDE and plot it
 xlabel="Projection on first eigenvector (nm)"
 ylabel="Projection on second eigenvector (nm)"
 
@@ -48,5 +45,4 @@ ax = sns.jointplot(x=df1.x1,
 	           marginal_ticks=False,
                    shade=True).set_axis_labels(xlabel, ylabel)
 
-plt.savefig('PCA_heatmap_E_e8.pdf')
-plt.savefig('PCA_heatmap_E_e8.png')
+plt.savefig('kdeplot.pdf')
