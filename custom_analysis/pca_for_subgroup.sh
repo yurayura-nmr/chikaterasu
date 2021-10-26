15ba01a9c4a5eab0a373843d6ee4d56ba8a53b83
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2021-10-26 13:10:25 +0900

    Update pca_for_subgroup.sh

--- custom_analysis/pca_for_subgroup.sh
+++ custom_analysis/pca_for_subgroup.sh
@@ -1,3 +1,5 @@
 gmx covar -s md_target.tpr -f md_fit.xtc -o pca/eigenval.xvg -av pca/average.pdb -n index.ndx
 
 gmx anaeig -s md_target.tpr -f md_fit.xtc -first 1 -last 3 -extr pca/extreme_pcavec.pdb -nframes 30 -entropy -v eigenvec.trr  -n index.ndx
+
+gmx anaeig -s md_target.tpr -f md_fit.xtc -2d pca/2dproj_12.xvg -first 1 -last 2 -n index.ndx
