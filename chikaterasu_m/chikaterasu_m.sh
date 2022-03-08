20ccafb18602d1811168986486d7b358ccd30216
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2022-03-08 16:48:05 +0900

    Add files via upload

---
+++ chikaterasu_m/chikaterasu_m.sh
@@ -0,0 +1,78 @@
+
+box_manual=true
+box_dim="8.000   8.000   8.000"
+protein_name="k48-CG"
+
+# top/itp must be manually prepared and ready in folder
+top="k48" # name
+
+# 1. editconf
+
+cd gromacs
+cd coord
+gmx editconf -f $protein_name.pdb -d 1.0 -bt triclinic -o $protein_name.gro
+
+if [ "$box_manual" = true ] ; then
+    sed -i '$ d' $protein_name.gro
+    echo "$box_dim" >> $protein_name.gro
+fi
+
+# 2. use top
+
+cd ../top/
+cp ../../chika_mdp/martini.itp .
+cp ../../*.top .
+cp ../../*.itp .
+
+gmx grompp -p $top.top -f ../../chika_mdp/minimization.mdp -c ../coord/$protein_name.gro -o ../emin/minimization-vac.tpr
+
+# Vacuum minimization
+cd ../emin/
+gmx mdrun -deffnm minimization-vac -v
+
+# 3. solvation
+
+cd ../solvation
+
+cp ../../chika_mdp/water.gro .
+gmx solvate -cp ../emin/minimization-vac.tpr -cs water.gro -radius 0.21 -o system_solvated.gro
+
+# Now we need to count how many waters we added
+cp ../top/$top.top ../top/system_solvated.top
+echo -n "W\t\t" >> ../top/system_solvated.top
+grep -c W system_solvated.gro >> ../top/system_solvated.top
+
+
+# Solution minimization
+cd ../emin
+gmx grompp -p ../top/system_solvated.top -c ../solvation/system_solvated.gro -f ../../chika_mdp/minimization.mdp -o minimization-sol.tpr
+gmx mdrun -deffnm minimization-sol -v
+
+# 4. Equilibration
+
+cd ../../runs/
+mkdir -p npt
+cd npt
+cp ../../gromacs/top/*.top .
+cp ../../gromacs/top/*.itp .
+cp ../../gromacs/emin/minimization-sol.gro .
+
+gmx grompp -p system_solvated.top -c minimization-sol -f ../../chika_mdp/equilibration.mdp -o equilibration.tpr
+gmx mdrun -deffnm equilibration -v 
+
+# 5. Production
+
+cd ..
+mkdir -p md
+cd md
+
+cp ../npt/*.top .
+cp ../npt/*.itp .
+cp ../npt/equilibration.gro .
+cp ../npt/equilibration.tpr .
+
+gmx grompp -p system_solvated.top -f ../../chika_mdp/dynamic.mdp -o dynamic.tpr -c equilibration.tpr -maxwarn 1
+#gmx grompp -p system_solvated.top -c equlibration.tpr -f ../../chika_mdp/dynamic.mdp -o dynamic.tpr -maxwarn 1
+gmx mdrun -deffnm dynamic -v 
+
+
