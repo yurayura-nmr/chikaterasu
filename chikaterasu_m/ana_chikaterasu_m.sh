8c6535424fcfb10af9f47673e090b1b40fee20b7
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2022-03-09 20:58:21 +0900

    Update ana_chikaterasu_m.sh

--- chikaterasu_m/ana_chikaterasu_m.sh
+++ chikaterasu_m/ana_chikaterasu_m.sh
@@ -1,5 +1,15 @@
 # very preliminary
 
+# ---- Visualization
+# to see motion. Center on protein and prevent jumping (for k48)
+gmx editconf -f ../dynamic.tpr -o target.pdb
+printf "non-Water\nq\n" | gmx make_ndx -f target.pdb -o target.ndx
+gmx convert-tpr -s ../dynamic.tpr  -n target.ndx -o md_target.tpr
+gmx trjconv -s ../dynamic.tpr -f ../dynamic.xtc -center -ur compact -pbc nojump -o md_target_centered_no_PBC.xtc
+gmx trjconv -s md_target.tpr -f md_target_centered_no_PBC.xtc -fit rot+trans -o md_fit.xtc
+gmx trjconv -s md_target.tpr -f md_target_centered_no_PBC.xtc -fit rot+trans -o test.pdb -conect
+# ----
+
 # on the fly check distance
 
 # make_ndx as usual
