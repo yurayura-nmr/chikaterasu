ec4d1213f9576bd1cfebf11d9ce1f543f2194209
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2022-03-30 16:18:34 +0900

    Update chikaterasu_m.sh

--- chikaterasu_m/chikaterasu_m.sh
+++ chikaterasu_m/chikaterasu_m.sh
@@ -3,9 +3,8 @@
 : '
 *************************************************************
 Chikaterasu_m       version dev
-gmx                 tested for gmx version 2019.2
-martini             tested for ff  version 2.2
-                    Older bash shells (2013) complain and cause errors.
+gmx                 tested for versions ....
+                    martini 2.2
                     
 Last change         see github
 
@@ -18,75 +17,41 @@ Graduate School of Medicine
 
 : '
 *************************************************************
-Assuming that the topology has already been set up using martinize
-http://www.cgmartini.nl/index.php/tutorials/37-tutorial2/311-tutorial-proteins#soluble-martini
-
-Example for K48 diUb
-python2.7 martinize.py -f 1aar_modified.pdb -o 1aar_modified.top -x 1aar_modified-CG.pdb -dssp dssp -p backbone -merge A,B -elastic -ef 500 -el 0.5 -eu 0.9 -ea 0 -ep 0
-
-To let bonds decay
-python2.7 martinize.py -f 1aar_modified.pdb -o 1aar_modified.top -x 1aar_modified-CG.pdb -dssp dssp -p backbone -merge A,B -elastic -ef 500 -el 0.5 -eu 0.9 -ea 1 -ep 1
-
-As an example, the difference between these two lines is:
-
--ea 0 -ep 0 
-    1    32      6   0.62818 RUBBER_FC*1.000000
-
--ea 1 -ep 1
-    1    32      6   0.62818 RUBBER_FC*0.879693
-
-
-To identify cross-bonds between subunits (we want to avoid these)
-awk -v x=163 '($1 <= x) && ($2 >x)' Protein_A+Protein_B.itp > to_replace.txt
-Open in vsCode side by side and comment out respective lines in Pro~Pro.itp
-
-Additional bonds that might be too stiff (C-terminus!)
-e.g.,
-  154   162      6   0.87282 RUBBER_FC*0.688791   ; R72-G75
-  154   163      6   0.76329 RUBBER_FC*0.768516   ; R72-G76
-  157   163      6   0.47946 RUBBER_FC*1.020747   ; L73-G76
-  
-To catch all of them ...
-   Distal:
-   77   154      6   0.85887 RUBBER_FC*0.698464   ; keeps R72 artificially in place
-   ...  154-157 etc.                              ; everything that links R72, L73 to the main core
-   
-   Proximal (everything past 315 (L71):
-   ...  315-326                                   ; holds R72 etc artificially close to P37 etc.
-
-
-Example for monoUb
-python2.7 martinize.py -f 1UBQ.pdb -o single-ubq.top -x 1UBQ-CG.pdb -dssp dssp -p backbone
-
-*************************************************************
-'
-: '
-*************************************************************
 Manually setup parameters for this run
-Most parameters are defined by mdp files in chika_mdp/
+Most parameters are defined by mdp files
 The starting PDB file should be placed into gromacs/coord
-
 Debug level 
 
-0   full production MD run (no debug)
-1   topology generation (editconf)
-2   solvation (solvate)
+// to be added
+0   full production MD run [NPT]; no debug
+1   topology generation [pdb2gmx]
+2   solvation
 3   addition of counterions
-4   equilibration before production
+    addition of distance restraints; no debug level implemented yet
+4   equilibration
 *************************************************************
 '
+
+# == How many runs ? ==
+
+nruns=10                    # 1 for testing; 10 for production
 
 # == Debug level ==
 
 debug_level=1               # Manually set debug level. Or give as argument, e.g.: ./chikaterasu 0
 
+
 # === Box shape and size ===
+
 box_manual=true
-box_dim="8.000   8.000   8.000"
+protein_name="k48-CG"
 
 # top/itp must be manually prepared and ready in folder
-top="k48"               # name
-protein_name="k48-CG"   # -CG coarse grained (martinized) pdb
+top="k48" # name
+
+box_manual=true
+box_dim="7.500   7.500   7.500"
+
 
 : '
 *************************************************************
@@ -108,10 +73,12 @@ rm -rf gromacs/top
 rm -rf gromacs/solvation
 rm -rf gromacs/addions
 rm -rf gromacs/emin
+#rm -rf gromacs/mdp   # unused?
 mkdir -p gromacs/top
 mkdir -p gromacs/solvation
 mkdir -p gromacs/addions
 mkdir -p gromacs/emin
+#mkdir -p gromacs/mdp
 mkdir -p gromacs/coord
 mkdir -p runs
 mkdir -p runs/nvt
@@ -119,7 +86,18 @@ mkdir -p runs/npt
 mkdir -p runs/md
 mkdir -p custom_analysis
 
+: '
+*************************************************************
+Assuming that the topology has already been set up using martinize
 
+Example for K48 diUb
+python2.7 martinize.py -f 1aar_modified.pdb -o 1aar_modified.top -x 1aar_modified-CG.pdb -dssp dssp -p backbone -merge A,B
+
+Example for monoUb
+python2.7 martinize.py -f 1UBQ.pdb -o single-ubq.top -x 1UBQ-CG.pdb -dssp dssp -p backbone
+
+*************************************************************
+'
 
 : '
 *************************************************************
@@ -129,10 +107,10 @@ Generate box around protein and perform 1 vacuum minimization
 *************************************************************
 '
 
-cd gromacs
-cd coord
-
 # Create box with editconf
+
+cd gromacs/coord
+
 gmx editconf -f $protein_name.pdb -d 1.0 -bt triclinic -o $protein_name.gro
 
 if [ "$box_manual" = true ] ; then
@@ -147,8 +125,9 @@ cp ../../chika_mdp/martini.itp .
 cp ../../*.top .
 cp ../../*.itp .
 
-# Vacuum minimization
 gmx grompp -p $top.top -f ../../chika_mdp/minimization.mdp -c ../coord/$protein_name.gro -o ../emin/minimization-vac.tpr
+
+# Vacuum minimization
 cd ../emin/
 gmx mdrun -deffnm minimization-vac -v
 
@@ -157,23 +136,23 @@ if [ "$debug_level" = 1 ] ; then
     exit 1
 fi
 
+
 : '
 *************************************************************
 Solvate the protein
 
-Then perform minization of solvated system
+Perform minization of solvated system
 *************************************************************
 '
 
 cd ../solvation
-cp ../../chika_mdp/water.gro .
 
-# Solvation
+cp ../../chika_mdp/water.gro .
 gmx solvate -cp ../emin/minimization-vac.tpr -cs water.gro -radius 0.21 -o system_solvated.gro
 
 # Now we need to count how many waters we added
 cp ../top/$top.top ../top/system_solvated.top
-echo -n "\nW\t\t" >> ../top/system_solvated.top
+echo -n "W\t\t" >> ../top/system_solvated.top
 grep -c W system_solvated.gro >> ../top/system_solvated.top
 
 # Solution minimization
@@ -186,6 +165,7 @@ if [ "$debug_level" = 2 ] ; then
     exit 1
 fi
 
+
 : '
 *************************************************************
 Add ions
@@ -196,63 +176,75 @@ Add ions
 
 # IONS NOT IMPLEMENTED YET
 
-# But seems to work mostly the same as normal gromacs
-
-# Pseudocode
-# get ions.mdp from chika_mdp
-# get martini_ions.itp and somehow include it in the topology
-# gmx grompp -f ./ions.mdp -c ../solvation/system_solvated.gro -p ../top/system_solvated.top -o ./ions.tpr    # -maxwarn 1
-# gmx genion -s ./ions.tpr -o solv_ions.gro -p ../top/system_solvated.top -conc 0 -neutral
-# and then continue with the solv ions.
-# check if topology updated with ions
-
-
 if [ "$debug_level" = 3 ] ; then
     echo "[Chikaterasu] Debug level 3 set. Exiting after adding counterions."
     exit 1
 fi
 
-: '
-*************************************************************
-Equilibration
-*************************************************************
-'
 
-cd ../../runs/
+cd ../..
 
-mkdir -p npt
-cd npt
-
-cp ../../gromacs/top/*.top .
-cp ../../gromacs/top/*.itp .
-cp ../../gromacs/emin/minimization-sol.gro .
-
-# Equilibration before production MD
-gmx grompp -p system_solvated.top -c minimization-sol -f ../../chika_mdp/equilibration.mdp -o equilibration.tpr
-gmx mdrun -deffnm equilibration -v 
-
-if [ "$debug_level" = 4 ] ; then
-    echo "[Chikaterasu] Debug level 4 set. Exiting after equilibration."
-    exit 1
-fi
 
 : '
 *************************************************************
-Production MD (only 1 run)
+Start the MD loop
 *************************************************************
 '
 
-cd ..
-mkdir -p md
-cd md
+for i in `seq 1 $nruns`;
 
-cp ../npt/*.top .
-cp ../npt/*.itp .
-cp ../npt/equilibration.gro .
-cp ../npt/equilibration.tpr .
+do
 
-# Run. Gromacs recognizes what to do with the GPU without specifically saying "-nb gpu"
-gmx grompp -p system_solvated.top -f ../../chika_mdp/dynamic.mdp -o dynamic.tpr -c equilibration.tpr -maxwarn 1
-gmx mdrun -deffnm dynamic -v 
+    # Equilibration
+    cd runs/
+    mkdir -p npt
+    cd npt
+    cp ../../gromacs/top/*.top .
+    cp ../../gromacs/top/*.itp .
+    cp ../../gromacs/emin/minimization-sol.gro .
+
+    gmx grompp -p system_solvated.top -c minimization-sol -f ../../chika_mdp/equilibration.mdp -o equilibration.tpr -maxwarn 2
+    gmx mdrun -deffnm equilibration -v 
+
+    if [ "$debug_level" = 4 ] ; then
+        echo "[Chikaterasu] Debug level 4 set. Exiting after equilibration."
+        exit 1
+    fi
+
+
+    # 5. Production
+    cd ..
+    mkdir -p md
+    cd md
+
+    cp ../npt/*.top .
+    cp ../npt/*.itp .
+    cp ../npt/equilibration.gro .
+    cp ../npt/equilibration.tpr .
+
+    gmx grompp -p system_solvated.top -f ../../chika_mdp/dynamic.mdp -o dynamic.tpr -c equilibration.tpr -maxwarn 2
+    #gmx grompp -p system_solvated.top -c equlibration.tpr -f ../../chika_mdp/dynamic.mdp -o dynamic.tpr -maxwarn 1
+    gmx mdrun -deffnm dynamic -nb gpu -v 
+    #gmx mdrun -deffnm dynamic -v
+
+    cd ../..
+   
+    # Copy the data
+    mkdir -p runs/md_$i
+    mkdir -p runs/nvt_$i
+    mkdir -p runs/npt_$i
+
+    mv runs/md/* runs/md_$i/
+    mv runs/nvt/* runs/nvt_$i/
+    mv runs/npt/* runs/npt_$i/
+
+    mkdir -p runs/md
+    mkdir -p runs/nvt
+    mkdir -p runs/npt
+
+    # End the run
+    echo [Chikaterasu] Run $i finished. Yay!
+
+done
 
 exit 1
