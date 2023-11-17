44c40b13291f1e59bc179c039ec2f94361e13eb5
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2023-11-17 12:54:40 +0900

    Update pca_for_subgroup.sh

--- custom_analysis/pca_for_subgroup.sh
+++ custom_analysis/pca_for_subgroup.sh
@@ -1,5 +1,13 @@
-gmx covar -s md_target.tpr -f md_fit.xtc -o pca/eigenval.xvg -av pca/average.pdb -n index.ndx
+# First, make the target by getting all backbone atoms and then excluding unwanted parts of the topology manually
+# gmx make_ndx -f ../md.tpr
 
-gmx anaeig -s md_target.tpr -f md_fit.xtc -first 1 -last 3 -extr pca/extreme_pcavec.pdb -nframes 30 -entropy -v eigenvec.trr  -n index.ndx
+echo "[ Chikaterasu ] PCA analysis will take a while ..."
+echo "[ Chikaterasu ] 1. Building covariance matrix ..."
 
-gmx anaeig -s md_target.tpr -f md_fit.xtc -2d pca/2dproj_12.xvg -first 1 -last 2 -n index.ndx
+gmx covar -s ../md_target.tpr -f ../md_fit.xtc -o ./eigenval.xvg -av ./average.pdb -n pca_target.ndx
+
+echo "[ Chikaterasu ] 2. Analyze first three eigenvectors ..."
+#gmx anaeig -s ../md_target.tpr -f ../md_fit.xtc -first 1 -last 3 -extr ./extreme_pcavec.pdb -nframes 30 -entropy -n pca_target.ndx
+
+echo "[ Chikaterasu ] 3. Plot first 3 PCA vectors as 2D maps ..."
+#gmx anaeig -s ../md_target.tpr -f ../md_fit.xtc -2d ./2dproj_12.xvg -first 1 -last 2 -n pca_target.ndx
