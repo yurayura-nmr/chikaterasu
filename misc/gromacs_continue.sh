fdf82d48b056e4c29325524e7f4a64cf653d0fb1
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2023-12-15 11:55:50 +0900

    Update gromacs_continue.sh

--- misc/gromacs_continue.sh
+++ misc/gromacs_continue.sh
@@ -2,4 +2,8 @@
 # Run within [chikaterasu]/runs/md folder
 
 gmx convert-tpr -s md.tpr -extend 0 -o md_continued.tpr
+
+# Time unit is picoseconds. So to extend by 100ns: -extend 100000
+# gmx convert-tpr -s md.tpr -extend 100000 -o md_continued.tpr
+
 gmx mdrun -s md_continued.tpr -cpi md.cpt -deffnm md -nb gp
