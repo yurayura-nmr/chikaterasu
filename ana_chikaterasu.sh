e805ed34193026c2861d172a05b401b65d97c0af
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2024-05-15 13:22:46 +0900

    Update ana_chikaterasu.sh
    Updated comments for hydrogen bond analysis.

--- ana_chikaterasu.sh
+++ ana_chikaterasu.sh
@@ -347,14 +347,21 @@ do
       cd hbond
       printf "MainChain+H\nMainChain+H" | gmx hbond -f ../md_target.xtc -s ../md_target.tpr -hbm -hbn hbond.ndx -num backbone_backbone.xvg -dt $dt
 
-      # (Note: this requires that plot_hbmap.pl is in the PATH and is executable)
-      # Extract hydrogen bonds in form of a nice table from the gmx hbond output using Justin's script
-      # Syntax of plot_hbmap.pl requires that the .ndx file
-      # contains only the [hbonds...] section (e.g. [ hbonds_MainChain+H ]) and only the atom numbers herein.
-      # Thus, edit ndx using awk so that only this section is in the data.
+      # *************************************************************
+      # Extract specific hydrogen bonds in table-form using Justin's script
+      # * Uses the accessory script plot_hbmap.pl. The script is provided in the custom_analysis directory.
+      # * Requires that plot_hbmap.pl is in the PATH and is executable. At present, Chikaterasu does not automatically copy that script here.
+
+      # [Caution]
+      # * Syntax of plot_hbmap.pl requires that the .ndx file (e.g., analyze.ndx) # contains only the [hbonds...] section (e.g. [ hbonds_MainChain+H ]) and only the atom numbers herein.
+      #   (so, there should be no other sections in this file; this would confuse the final output).
+      # * To achieve that, we use awk to so that only this section is in the data is in the ndx that will be the input for the perl script.
       awk '/hbond/{y=1;next}y' hbond.ndx > analyze.ndx
       plot_hbmap.pl -s ../target.pdb -map hbmap.xpm -index analyze.ndx
-      
+
+      # *************************************************************
+
+      # Continue with other hydrogen bond analyses ...
       printf "Protein\nProtein" | gmx hbond -f ../md_target.xtc -s ../md_target.tpr -hbm -hbn -num protein_protein.xvg -dt $dt
 
       #printf "Protein\nWater" | gmx hbond -f md_full.xtc -s md.tpr -num hbond/protein_water.xvg
