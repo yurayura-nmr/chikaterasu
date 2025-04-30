64952fe51c40dfae85ab24e37ed14664cff57576
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2025-04-30 16:53:52 +0900

    Update ase2pdb.py

--- misc/ase2pdb.py
+++ misc/ase2pdb.py
@@ -8,3 +8,8 @@ traj = read('example.traj', index=':')
 
 # Write to PDB format (suitable for GROMACS input)
 write('example.pdb', traj)
+
+
+# Better for pymol - convert to dcd format
+# traj2dcd.py is found in ai2bmbd github /src/utils
+python3 traj2dcd.py --input ../Logs-chig/chig-traj.traj --output test.dcd --pdb ../chig_preprocessed/chig-preeq.pdb --stride 1
